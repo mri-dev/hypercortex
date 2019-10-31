ca5b42456c123c67fbc0aa7b8b4cd848e0497df6
mri-dev <pocokxtramepocokxtrame@gmail.com>
2019-10-31 17:43:43 +0100

    Save

--- wp-content/themes/Avada-Child-Theme/templates/shortcodes/Testimonials.php
+++ wp-content/themes/Avada-Child-Theme/templates/shortcodes/Testimonials.php
@@ -1,9 +1,27 @@
 <div class="wrapper">
   <?php if ($posts->have_posts()): ?>
+    <div class="partnerek">
     <?php  while ( $posts->have_posts() ) {
       $posts->the_post();
-      $img = get_the_post_thumbnail_url(get_the_ID()); echo $img; ?>
-
+      $img = get_the_post_thumbnail_url(get_the_ID());
+    ?>
+    <div class="partner">
+      <img src="<?=$img?>" alt="<? echo the_title(); ?>" title="<? echo the_title(); ?>">
+    </div>
     <? } ?>
-  <?php wp_reset_postdata(); endif; ?>
+  </div>
+  <script type="text/javascript">
+    (function($){
+      $(function(){
+        $('.partnerek').slick({
+          infinite: true,
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          arrows: true
+        });
+      });
+    })(jQuery);
+  </script>
+  <?php wp_reset_postdata();?>
+<?php endif; ?>
 </div>
